ca9caced4d6389b0afc89b36bc77a046d7c9af51
Charles-Rocke <89711262+Charles-Rocke@users.noreply.github.com>
2023-07-01 11:35:23 +0000

    formsss

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -26,26 +26,31 @@ function Form() {
 		<section className="py-md-0 bg-hero" id="home">
 			<div className="container">
 				<div className="col-md-8 col-sm-10 col-12 mx-auto my-auto text-center">
-					<form name="waitlist v1" method="POST" data-netlify="true" className="form-split-bill" onSubmit="submit">
+					
+					<form name="waitlist v1" method="POST" data-netlify="true" className="form-split-bill" onSubmit={handleSubmit}>
+						
 						<input type="hidden" name="waitlist v1" value="waitlist v1" />
+						
 						<div className="mb-3 label-div">
-							<label htmlFor="name" for="exampleFormControlInput1" className="form-label">Your name</label>
+							<label htmlFor="name" className="form-label">Your name</label>
 							<input id="name" name="name" value={name} onChange={(event) => setName(event.target.value)} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your name" />
 						</div>
 
 						<div className="mb-3 label-div">
-							<label htmlFor="email" for="exampleFormControlInput1" className="form-label">Email address</label>
+							<label htmlFor="email" className="form-label">Email address</label>
 							<input id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
 						</div>
 
 						<div className="mb-3 label-div">
-							<label htmlFor="city" for="exampleFormControlInput1" className="form-label">Your City</label>
+							<label htmlFor="city" className="form-label">Your City</label>
 							<input id="city" name="city" value={city} onChange={(event) => setCity(event.target.value)} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your city" />
 						</div>
-						<Button type="submit" onClick={handleSubmit}>
+						
+						<Button type="submit">
 							Join waitlist
 							<em className="ml-2"></em>
 						</Button>
+						
 					</form>
 				</div>
 			</div>
